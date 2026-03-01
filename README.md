6466b80eb831661b02ce00f4d8f9b985bd469677
charmutune <mutunechar@gmail.com>
2026-03-02 00:39:26 +0300

    Update and rename README.md to hello_world.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-basics
-Python fundamentals and practice exercises as I begin my cybersecurity journey.
---
+++ hello_world.py
@@ -0,0 +1,23 @@
+# python-basics
+Python fundamentals and practice exercises as I begin my cybersecurity journey.
+## Topics Covered
+- Variables and Data Types
+- Conditionals
+- Loops
+- Functions
+- Basic Scripts
+
+## Goal
+To develop strong programming skills for cybersecurity and data analysis.
+
+---
+
+Author: Char
+
+# My first Python script
+
+name = "Char"
+career_goal = "Cybersecurity Professional"
+
+print("Hello, my name is", name)
+print("I am training to become a", career_goal)
